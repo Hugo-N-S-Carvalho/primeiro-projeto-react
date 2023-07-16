d9027c47188ad5b217e1c2568bb1de3e7ce32a45
Hugo-N-S-Carvalho <huggones@hotmail.com>
2023-07-16 18:36:24 -0300

    Hello React PPReact

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+  return (
 
-  return <div>
+<div>
 
 Hello React
 
+  
+</div>
 
 
-  </div>;
+  )
+    
+      
+        
+    
+  
 }
 
-export default App;
+export default App
